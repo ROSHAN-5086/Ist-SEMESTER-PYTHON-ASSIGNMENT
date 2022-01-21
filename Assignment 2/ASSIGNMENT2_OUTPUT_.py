17794127119da38b7022215fd162203bb3af03bd
Roshan Tripathi <96870171+RoshanTripathi@users.noreply.github.com>
2022-01-21 22:26:00 +0530

    Add files via upload

---
+++ Assignment 2/ASSIGNMENT2_OUTPUT_.py
@@ -0,0 +1,47 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/roshantripathi/Desktop/Program/PEC assignment/ASSIGNMENT2_21105086_.py
+
+QUESTION-1
+Enter the string:Python is a case sensitive language
+a. Length of 'Python is a case sensitive language' is 35
+b. 'egaugnal evitisnes esac a si nohtyP' is reversed string
+c. New string is 'a case sensitive'
+d. Replaced string is 'object oriented'
+e. Index of substring 'a' in given input string is: 10
+f. String after removing white spaces is 'Pythonisacasesensitivelanguage'
+
+Question-2
+Enter your name:Roshan
+Enter your SID:21105086
+Enter your department name:ECE
+Enter your CGPA:9.8
+Hey, Roshan Here! My SID is 21105086 I am from ECE department and my CGPA is 9.8
+
+Question-3
+a=56
+b=10
+A. a&b is 8
+B. a|b is 58
+C. a^b is 50
+D1. Left shift a with 2 bits  is 224
+D2. Left shift b with 2 bits is 40
+E1. Right shift a with 2 bits is 14
+E2. Left shift b with 4 bits is 0
+
+Question-4
+Enter first number:23
+Enter second number:45.8
+Enter third number:34.6
+45.8 is greatest of all numbers entered.
+
+Question-5
+Enter a Statement:Hello my name is ROSHAN
+YES 'name' word is present in 'Hello my name is ROSHAN'
+
+Question-6
+Enter length of 1st side of triangle:3
+Enter length of 2nd side of triangle:4
+Enter length of 3rd side of triangle:5
+YES given sides 3,4,5 can form a triangle
