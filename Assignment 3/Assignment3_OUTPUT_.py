fd57d53bd1a22b4944a7bcd1cf9975e947b17826
Roshan Tripathi <96870171+RoshanTripathi@users.noreply.github.com>
2022-02-04 22:08:08 +0530

    Add files via upload

---
+++ Assignment 3/Assignment3_OUTPUT_.py
@@ -0,0 +1,108 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/roshantripathi/Desktop/Program/PEC assignment/Assignment3_21105086_.py
+
+Q.1
+Enter the string:Hello my name is Roshan and my branch is ECE
+
+Dictionary containing {'Word':'Word Count'} is shown below:
+{'roshan': 1, 'ece': 1, 'name': 1, 'my': 2, 'is': 2, 'hello': 1, 'and': 1, 'branch': 1}
+
+Q.2
+Enter Date below to get date of next day.
+
+Enter Day [1-31]:30
+Enter Month [1-12]:12
+Enter Year [1800-2025]:2000
+
+Next Date in format Day/Month/Year is: 1/1/2001
+
+Q.3
+Enter the numbers separated by space:1 2 3 4 5 6 7 8 9
+
+List containing (n,n^2) is shown below:
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81)]
+
+Q.4
+Enter grade points:9
+Your grade is 'A' and Excellent performance.
+
+Q.5
+The pattern is printed below:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+Q.6
+Enter student name:Roshan
+Enter student SID:21105086
+Enter Y to continue or N to end:y
+Enter student name:Hritik
+Enter student SID:21105085
+Enter Y to continue or N to end:y
+Enter student name:Prayag
+Enter student SID:21105099
+Enter Y to continue or N to end:y
+Enter student name:Harsh
+Enter student SID:21105012
+Enter Y to continue or N to end:n
+
+Q.6(a)
+The Dictionary containing {'SID':'Name'} is shown below
+{21105086: 'Roshan', 21105085: 'Hritik', 21105099: 'Prayag', 21105012: 'Harsh'}
+
+Q.6(b)
+The Dictionary after sorting according to name:
+{21105012: 'Harsh', 21105085: 'Hritik', 21105099: 'Prayag', 21105086: 'Roshan'}
+
+Q.6(c)
+The Dictionary after sorting according to SID:
+{21105012: 'Harsh', 21105085: 'Hritik', 21105086: 'Roshan', 21105099: 'Prayag'}
+
+Q.6(d)
+Enter SID to get name of student:21105086
+Name of student with SID 21105086 is Roshan.
+
+Q.7
+Enter number of elements N in fibonacci series:
+[N must be positive Integer]: N=15
+
+The fibonacci series with 15 elements is shown below:
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+
+Average of given fibonacci series is 106.40
+
+Q.8
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+Q.8(a)
+
+A new Set of all 'elements that are in Set1 and Set2 but not both' is:
+{1, 3, 5, 6, 8}
+
+Q.8(b)
+
+A new set of all elements that are 'in only one of the three sets Set1,Set2 and Set3' is:
+{3, 6, 8, 9, 13, 17}
+
+Q.8(c)
+
+A new set of elements that are 'exactly in two of the sets Set1, Set2 and Set3' is:
+{1, 2, 4, 5}
+
+Q.8(d)
+
+A new set of all Integers in the 'range 1 to 10' that are 'not in Set1' is:
+{6, 7, 8, 9, 10}
+
+Q.8(e)
+
+A new set of all Integers in the range 1 to 10 that are not in Set1,Set2 and Set3.
+{10, 7}
